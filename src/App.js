9ec924856a3b8de35b94251ed131951800784f51
mabpg <mabpen195@gmail.com>
2020-06-23 11:48:16 -0400

    DateInput ahora es un Class component y le agregué el submit Random.

--- src/App.js
+++ src/App.js
@@ -7,35 +7,46 @@ import "bootswatch/dist/solar/bootstrap.min.css";
 class App extends Component {
   state = {
     date: "",
-    photo: ""
+    photo: "",
+    rndDate: ""
   };
 
-  changeDate = e => {
-    e.preventDefault();
-    let dateFromInput = e.target[0].value; //date del state
-    this.setState({ date: dateFromInput });
-    this.getPhoto(dateFromInput);    
-  };
+  dateSearch = (termino) => {
+    //console.log("termino recibido: " + termino);
+    if(termino!=="rnd"){ //invocado desde el submit search
+      this.setState({ date: termino });
+      this.getPhoto(termino);
+    }else {
+      //invocado desde el submit random
+      this.getRndPhoto();
+      console.log("Invoke from Random " + this.state.rndDate)
+    } 
+  }
 
   componentDidMount() {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    fetch('https://api.nasa.gov/planetary/apod?api_key=9maQXMYRXnmlPmw5qc2L78HaDaw2zEdb4MSMBstG')
       .then(response => response.json())
       .then(json => this.setState({ photo: json }));
   }
 
   getPhoto = date => {
-    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=DEMO_KEY`)
+    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=9maQXMYRXnmlPmw5qc2L78HaDaw2zEdb4MSMBstG`)
       .then(response => response.json())
       .then(photoData => this.setState({ photo: photoData }));
   };
 
-   
+  getRndPhoto = e => {
+    fetch('http://localhost:8001/randomdate')
+      .then(response => response.json())
+      .then(rndDate => this.setState({ rndDate: rndDate }));
+  };
+
   render() {
     return (
       <div className="apod container">
         <div className="jumbotron">
           <h1 className="text-white text-center">NASA's Astronomy Picture of the Day</h1>
-          <DateInput changeDate={this.changeDate}/>
+          <DateInput dateSearch={this.dateSearch}/>
           <Photo photo={this.state.photo}/>
         </div>
       </div>
--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,20 +1,47 @@
-//functional component
-import React from "react";
+//Class component
+import React, { Component } from "react";
 
-const DateInput = props => (
-    <form onSubmit={props.changeDate}>
-        <label className="col-form-label" htmlFor="inputLarge">Enter a date:</label>
-        <div className="row">
+class DateInput extends Component {
+
+    searchRef = React.createRef();
+
+    changeSearchDate = (e) => {
+        e.preventDefault();
         
-            <div className="form-group col-md-8">                
-                <input type="text" className="form-control form-control-lg" placeholder="YYYY-MM-DD"/>
-                               
-            </div>
-            <div className="form-group col-md-4">
-                <input type="submit" className="btn btn-lg btn-primary btn-block" value="Search"/>
-            </div>
-        </div>       
-        
-    </form>
-  );
+        const termino = this.searchRef.current.value;
+        this.props.dateSearch(termino);
+    };
+
+    changeRndDate = (e) => {
+        e.preventDefault();  
+        this.props.dateSearch("rnd");
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.changeSearchDate}>
+                <label className="col-form-label" htmlFor="inputLarge">Enter a date:</label>
+                <div className="row">                
+                    <div className="form-group col-md-8">                
+                        <input type="text" ref={this.searchRef} id="fecha" className="form-control form-control-lg" placeholder="YYYY-MM-DD"/>
+                                    
+                    </div>
+                    <div className="form-group col-md-2">
+                        <input type="submit" className="btn btn-lg btn-primary btn-block" value="Search"/>
+                    </div>
+                    
+                </div>       
+                <div className="form-group col-md-2" >
+                    <input type="submit" onClick={this.changeRndDate} className="btn btn-lg btn-info btn-block" value="Random"/>
+                </div>
+            </form>
+            
+               
+            
+        );
+    }
+
+
+}
+
 export default DateInput;
